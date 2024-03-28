d79aa569db03a98a13c70cbe427f0dc3401156f2
Akhil Bille <akhilgowd829@gmail.com>
2024-03-28 07:23:02 -0400

    Basics of Object

---
+++ J8Object.js
@@ -0,0 +1,14 @@
+//Object: That exists in real life
+//Eg: A person is an object who has details like name,age,emailID
+//Objects will helps to group highly related data into one object
+// Syntax to create an object
+let person = {
+    name : 'Akhil',
+    age:23
+}
+
+// Accessing prperties of an object
+console.log(person.age);
+
+//Updating a property of an object
+person.name = 'Akhil Bille';
